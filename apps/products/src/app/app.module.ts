cec9cfc9c403025a7f13a9703659fe51e61211a9
rmonteroh <rmonteroh90@gmail.com>
2022-02-02 15:51:04 -0500

    Last changes

--- apps/products/src/app/app.module.ts
+++ apps/products/src/app/app.module.ts
@@ -10,12 +10,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path: 'products',
+        loadChildren: () =>
+          import('./remote-entry/entry.module').then((m) => m.RemoteEntryModule),
+      },
+    ], { initialNavigation: 'enabledBlocking' }),
   ],
   providers: [],
   bootstrap: [AppComponent],
